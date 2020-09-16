1ae535181d700fd5fbd2421e383577f1047aeeb7
Mykhailo Pysariev <m.pysarev@gmail.com>
2020-09-16 13:21:58 +0300

    small changes to content file

--- ht-1/src/Content.js
+++ ht-1/src/Content.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Body extends React.Component {
+export default class Content extends React.Component {
     render() {
         return <div className="content">Body content</div>
     }
